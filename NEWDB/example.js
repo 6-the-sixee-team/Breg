7f71f43172e1142723924ccda2fe521152d79d02
Breg Bromley <bregbromley@gmail.com>
2020-05-26 18:25:39 -0600

    sample results of core db querie

---
+++ NEWDB/example.js
@@ -0,0 +1,301 @@
+// SELECT * FROM reviews WHERE product_id=3 AND user_id=4;
+
+[
+  {
+    'review_id': 22,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Refined',
+    'text': 'In optio voluptas quo quasi ullam.',
+    'doesrecommend': false,
+    'created_at': '2020-01-14T07:00:00.000Z'
+  },
+  {
+    'review_id': 42,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Sleek',
+    'text': 'Quia repellat et maxime.',
+    'doesrecommend': true,
+    'created_at': '2020-05-02T06:00:00.000Z'
+  },
+  {
+    'review_id': 53,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Intelligent',
+    'text': 'Provident harum ipsam.',
+    'doesrecommend': false,
+    'created_at': '2020-04-30T06:00:00.000Z'
+  },
+  {
+    'review_id': 92,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Practical',
+    'text': 'Accusamus culpa quo vitae dolor consequatur ipsum tenetur iure.',
+    'doesrecommend': false,
+    'created_at': '2020-01-10T07:00:00.000Z'
+  },
+  {
+    'review_id': 96,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Licensed',
+    'text': 'Quos velit fugiat officia nemo.',
+    'doesrecommend': false,
+    'created_at': '2020-01-25T07:00:00.000Z'
+  },
+  {
+    'review_id': 131,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Practical',
+    'text': 'Hic ut at eius.',
+    'doesrecommend': true,
+    'created_at': '2020-04-18T06:00:00.000Z'
+  },
+  {
+    'review_id': 182,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Tasty',
+    'text': 'Ea et amet aut tenetur corrupti.',
+    'doesrecommend': false,
+    'created_at': '2020-02-15T07:00:00.000Z'
+  },
+  {
+    'review_id': 224,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Practical',
+    'text': 'Itaque et aut saepe provident consectetur unde sint sequi.',
+    'doesrecommend': true,
+    'created_at': '2020-01-25T07:00:00.000Z'
+  },
+  {
+    'review_id': 230,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Small',
+    'text': 'Optio eum explicabo a.',
+    'doesrecommend': true,
+    'created_at': '2020-01-12T07:00:00.000Z'
+  },
+  {
+    'review_id': 233,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Handcrafted',
+    'text': 'Qui eum quia delectus quis exercitationem dolorem aut voluptate fuga.',
+    'doesrecommend': false,
+    'created_at': '2020-02-03T07:00:00.000Z'
+  },
+  {
+    'review_id': 238,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Practical',
+    'text': 'Adipisci et optio doloremque ut.',
+    'doesrecommend': false,
+    'created_at': '2020-02-29T07:00:00.000Z'
+  },
+  {
+    'review_id': 295,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Generic',
+    'text': 'Voluptas quam quod eius rerum amet tempore.',
+    'doesrecommend': true,
+    'created_at': '2020-03-01T07:00:00.000Z'
+  },
+  {
+    'review_id': 381,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Tasty',
+    'text': 'Incidunt ea architecto soluta optio.',
+    'doesrecommend': true,
+    'created_at': '2020-03-23T06:00:00.000Z'
+  },
+  {
+    'review_id': 426,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Small',
+    'text': 'Maxime fugiat commodi ipsam.',
+    'doesrecommend': false,
+    'created_at': '2020-03-01T07:00:00.000Z'
+  },
+  {
+    'review_id': 477,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Handmade',
+    'text': 'Sequi aliquid omnis ut.',
+    'doesrecommend': true,
+    'created_at': '2020-04-25T06:00:00.000Z'
+  },
+  {
+    'review_id': 509,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Ergonomic',
+    'text': 'Reiciendis error dolor molestiae nihil voluptatibus asperiores laudantium.',
+    'doesrecommend': true,
+    'created_at': '2020-03-12T06:00:00.000Z'
+  },
+  {
+    'review_id': 536,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Tasty',
+    'text': 'Consequuntur sit iure.',
+    'doesrecommend': false,
+    'created_at': '2020-02-20T07:00:00.000Z'
+  },
+  {
+    'review_id': 556,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Unbranded',
+    'text': 'Officiis quasi dolorem.',
+    'doesrecommend': true,
+    'created_at': '2020-01-07T07:00:00.000Z'
+  },
+  {
+    'review_id': 575,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Licensed',
+    'text': 'Magni autem impedit.',
+    'doesrecommend': true,
+    'created_at': '2020-02-19T07:00:00.000Z'
+  },
+  {
+    'review_id': 625,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Gorgeous',
+    'text': 'Est fugiat consequuntur ullam atque aut ea.',
+    'doesrecommend': false,
+    'created_at': '2020-02-13T07:00:00.000Z'
+  },
+  {
+    'review_id': 657,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Fantastic',
+    'text': 'Voluptatibus harum maiores consequatur ut temporibus ab vel similique perspiciatis.',
+    'doesrecommend': false,
+    'created_at': '2020-01-11T07:00:00.000Z'
+  },
+  {
+    'review_id': 734,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Awesome',
+    'text': 'Odio accusantium qui esse.',
+    'doesrecommend': true,
+    'created_at': '2020-02-21T07:00:00.000Z'
+  },
+  {
+    'review_id': 737,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Fantastic',
+    'text': 'Officiis et nemo excepturi sunt modi dolorum omnis optio.',
+    'doesrecommend': true,
+    'created_at': '2020-02-02T07:00:00.000Z'
+  },
+  {
+    'review_id': 803,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Unbranded',
+    'text': 'Quasi et magni dolores.',
+    'doesrecommend': false,
+    'created_at': '2020-04-26T06:00:00.000Z'
+  },
+  {
+    'review_id': 820,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Rustic',
+    'text': 'Quia voluptas deserunt quis ad et.',
+    'doesrecommend': true,
+    'created_at': '2020-02-14T07:00:00.000Z'
+  },
+  {
+    'review_id': 845,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Handmade',
+    'text': 'Earum laudantium nihil mollitia eligendi rerum inventore doloribus vero.',
+    'doesrecommend': true,
+    'created_at': '2020-02-28T07:00:00.000Z'
+  },
+  {
+    'review_id': 862,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Practical',
+    'text': 'Molestiae officiis quisquam vero aut doloremque.',
+    'doesrecommend': false,
+    'created_at': '2020-02-21T07:00:00.000Z'
+  },
+  {
+    'review_id': 898,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Incredible',
+    'text': 'Architecto reiciendis necessitatibus voluptatem ex necessitatibus ab ut aperiam culpa.',
+    'doesrecommend': true,
+    'created_at': '2020-03-25T06:00:00.000Z'
+  },
+  {
+    'review_id': 926,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Sleek',
+    'text': 'Autem totam reiciendis nisi ad placeat dolor iure et.',
+    'doesrecommend': false,
+    'created_at': '2020-03-14T06:00:00.000Z'
+  },
+  {
+    'review_id': 927,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Unbranded',
+    'text': 'Quaerat qui facere totam accusantium et et ad.',
+    'doesrecommend': false,
+    'created_at': '2020-02-18T07:00:00.000Z'
+  },
+  {
+    'review_id': 956,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Generic',
+    'text': 'Quis earum id dolores accusamus ab.',
+    'doesrecommend': true,
+    'created_at': '2020-01-09T07:00:00.000Z'
+  },
+  {
+    'review_id': 965,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Generic',
+    'text': 'Vitae accusantium voluptatem veritatis error.',
+    'doesrecommend': true,
+    'created_at': '2020-04-03T06:00:00.000Z'
+  },
+  {
+    'review_id': 984,
+    'product_id': 3,
+    'user_id': 4,
+    'title': 'Incredible',
+    'text': 'Odit ad consequuntur accusamus aut deleniti assumenda.',
+    'doesrecommend': true,
+    'created_at': '2020-03-10T06:00:00.000Z'
+  }
+];
